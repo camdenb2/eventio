1e4de05aad4cfee11f4ed0a7ac4a465e45414bd7
Camden Birkeland <camdenb2@aim.com>
2016-11-02 19:02:38 -0600

    Added Edit & Delete options

--- events/views.py
+++ events/views.py
@@ -1,4 +1,7 @@
+from django.contrib import messages
 from django.shortcuts import redirect, render, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.core.exceptions import PermissionDenied
 
 from .forms import AddEventForm
 from .models import Event
@@ -11,12 +14,43 @@ def event_detail(request, pk):
     event = get_object_or_404(Event, pk=pk)
     return render(request, 'event_detail.html', {'event': event})
 
+@login_required
 def event_add(request):
     if request.method == 'POST':
         form = AddEventForm(request.POST)
         if form.is_valid():
-            event = form.save()
+            event = form.save(commit=False)
+            event.owner = request.user
+            event.save()
             return redirect('event', pk=event.pk)
     else:
         form = AddEventForm()
     return render(request, 'event_add.html', {'form': form})
+
+@login_required
+def event_delete(request, pk):
+    event = get_object_or_404(Event, pk=pk)
+
+    if request.user != event.owner:
+        raise PermissionDenied
+
+    event.delete()
+    messages.success(request, 'Event Deleted')
+    return redirect('/')
+
+
+@login_required
+def event_edit(request, pk):
+    event = get_object_or_404(Event, pk=pk)
+
+    if request.user != event.owner:
+        raise PermissionDenied
+
+    if request.method == 'POST':
+        form = AddEventForm(request.POST, instance=event)
+        if form.is_valid():
+            event = form.save()
+            return redirect('event', pk=event.pk)
+    else:
+        form = AddEventForm(instance=event)
+    return render(request, 'event_edit.html', {'form': form})
